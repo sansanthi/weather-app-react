19e527b0b74c151beeb07bdd906a3a4c3f210614
San Thi <sansanthi1996@gmail.com>
2023-07-01 15:10:09 +0630

    added link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <main>
       <Weather />
+      <p className="text-center link">
+        This project is coded by{" "}
+        <a href="https://www.shecodes.io/graduates/73791-san-san-thi">
+          San San Thi
+        </a>
+        and is open-sourced on{" "}
+        <a href="https://github.com/sansanthi/weather-app-react">GitHub</a>
+      </p>
     </main>
   );
 }
